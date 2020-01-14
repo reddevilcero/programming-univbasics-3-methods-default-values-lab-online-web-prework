be6f31520bcb3fee088e47023691966314a8aa4c
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-14 10:21:39 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,7 +5,7 @@ def meal_choice(veg1, veg2, protein='meat')
 
   puts "What a nutritious meal!"
 
-  message = "A plate of #{protein} with #{veg1} and #{veg2}"
+  message = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts message
 
   return message
